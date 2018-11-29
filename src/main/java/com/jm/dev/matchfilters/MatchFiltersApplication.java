f1f4fe3fa236ba76613c99f4ca9046e55285681d
jmdev <jmdev09@gmail.com>
2018-11-29 13:48:10 +0530

    Initial Commit
    Initial Commit SpringBoot App

---
+++ src/main/java/com/jm/dev/matchfilters/MatchFiltersApplication.java
@@ -0,0 +1,12 @@
+package com.jm.dev.matchfilters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchFiltersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchFiltersApplication.class, args);
+	}
+}
